16870d24f497e3e5daf0fef45ba5168aa1518c7b
Anne <northofnormal@gmail.com>
2017-05-07 22:28:51 -0400

    add stuff

--- CocktailGenerator/Networking.swift
+++ CocktailGenerator/Networking.swift
@@ -14,53 +14,57 @@ class JSONParser {
     let indexURLString = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
     let drinkURLBaseString = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
     
-    // remember this: http://stackoverflow.com/questions/27390656/how-to-return-value-from-alamofire
-    // specifically, the "orders" parameter
-    
-    // name this better
-    func fetchIndex(handler: @escaping ([AnyObject]?) -> ()) {
-        makeCall(indexURLString, completionHandler: handler)
-    }
-    
-    func fetchDrink(id: String, handler: @escaping ([AnyObject]?) -> ()) {
-        makeCall(drinkURLBaseString + id, completionHandler: handler)
-    }
-    
-    func makeCall(_ url: String, completionHandler: @escaping ([AnyObject]) -> ()) {
-        
-        Alamofire.request(url, method: .get).responseJSON { response in
-            
-            switch url {
-            case self.indexURLString:
-                var drinkIndex: [DrinkIndex] = []
-                if let cocktailJSON = response.result.value as? [String: AnyObject] {
-                    if let cocktails: [[String: Any]] = cocktailJSON["drinks"] as? [[String : Any]] {
-                        drinkIndex = cocktails.flatMap {cocktail in
-                            if let cocktailName: String = cocktail["strDrink"] as? String,
-                                let cocktailID: String = cocktail["idDrink"] as? String {
-                                let cocktailIndex = DrinkIndex(name: cocktailName, id: cocktailID)
-                                return cocktailIndex
-                            }
-                            return nil
+    func fetchIndex(handler: @escaping ([DrinkIndex]?) -> ()) {
+        Alamofire.request(indexURLString, method: .get).responseJSON { response in
+            var drinkIndex: [DrinkIndex] = []
+            if let cocktailJSON = response.result.value as? [String: AnyObject] {
+                if let cocktails: [[String: Any]] = cocktailJSON["drinks"] as? [[String : Any]] {
+                    drinkIndex = cocktails.flatMap {cocktail in
+                        if let cocktailName: String = cocktail["strDrink"] as? String,
+                            let cocktailID: String = cocktail["idDrink"] as? String {
+                            let cocktailIndex = DrinkIndex(name: cocktailName, id: cocktailID)
+                            return cocktailIndex
                         }
+                        return nil
                     }
-                } else {
-                    print("there's no JSON 😶")
                 }
-                
-                completionHandler(drinkIndex as [AnyObject])
-                return
-                // well this doesn't work bc the URL includes the id part now :facepalm: 
-                // but I am hitting the correct url and getting back the expected response. 
-            case self.drinkURLBaseString:
-                print(response.result.value)
-                return
-                
-            default: break
+            } else {
+                print("there's no JSON 😶")
             }
             
-            
+            handler(drinkIndex)
         }
     }
     
+    func fetchDrink(id: String, handler: @escaping (Drink?) -> ()) {
+        Alamofire.request(drinkURLBaseString + id, method: .get).responseJSON { response in
+            if let responseJSON = response.result.value as? [String: AnyObject] {
+                if let drinkArray: [[String: Any]] = responseJSON["drinks"] as? [[String: Any]] {
+                    let drink: Drink = drinkArray.flatMap { thing in
+                        if let drinkName: String =  thing["strName"] as? String,
+                            let drinkID: String = thing["idDrink"] as? String,
+                            let drinkGlasss: String = thing["strGlass"] as? String,
+                            let drinkInstructions: String = thing["strInstructions"] as? String,
+                            let drinkIng1: String = thing["strIngredient1"] as? String,
+                            let drinkIng2: String = thing["strIngredient2"] as? String,
+                            let drinkIng3: String = thing["strIngredient3"] as? String,
+                            let drinkIng4: String = thing["strIngredient4"] as? String,
+                            let drinkIng5: String = thing["strIngredient5"] as? String,
+                            let drinkMeasure1: String = thing["strMeasure1"] as? String,
+                            let drinkMeasure2: String = thing["strMeasure2"] as? String,
+                            let drinkMeasure3: String = thing["strMeasure3"] as? String,
+                            let drinkMeasure4: String = thing["strMeasure4"] as? String,
+                            let drinkMeasure5: String = thing["strMeasure5"] as? String {
+                                return Drink(name: drinkName, id: drinkID, glass: drinkGlasss, instructions: drinkInstructions, ingredients1: drinkIng1, ingredients2: drinkIng2, ingredients3: drinkIng3, ingredients4: drinkIng4, ingredients5: drinkIng5, measure1: drinkMeasure1, measure2: drinkMeasure2, measure3: drinkMeasure3, measure4: drinkMeasure4, measure5: drinkMeasure5)
+                        }
+                    }
+                    return drink
+                }
+            } else {
+                print("there's no JSON 😶")
+            }
+            
+        }
+    }
 }
+
--- DrinkViewController.swift
+++ DrinkViewController.swift
@@ -24,33 +24,13 @@ struct Drink {
     var ingredient3: String
     var ingredient4: String
     var ingredient5: String
-    var ingredient6: String
-    var ingredient7: String
-    var ingredient8: String
-    var ingredient9: String
-    var ingredient10: String
-    var ingredient11: String
-    var ingredient12: String
-    var ingredient13: String
-    var ingredient14: String
-    var ingredient15: String
     var measure1: String
     var measure2: String
     var measure3: String
     var measure4: String
     var measure5: String
-    var measure6: String
-    var measure7: String
-    var measure8: String
-    var measure9: String
-    var measure10: String
-    var measure11: String
-    var measure12: String
-    var measure13: String
-    var measure14: String
-    var measure15: String
     
-    public init(name: String, id: String, glass: String, instructions: String, ingredients1: String, ingredients2: String, ingredients3: String, ingredients4: String, ingredients5: String, ingredients6: String, ingredients7: String, ingredients8: String, ingredients9: String, ingredients10: String, ingredients11: String, ingredients12: String, ingredients13: String, ingredients14: String, ingredients15: String, measure1: String, measure2: String, measure3: String, measure4: String, measure5: String, measure6: String, measure7: String, measure8: String, measure9: String, measure10: String, measure11: String, measure12: String, measure13: String, measure14: String, measure15: String) {
+    public init(name: String, id: String, glass: String, instructions: String, ingredients1: String, ingredients2: String, ingredients3: String, ingredients4: String, ingredients5: String, measure1: String, measure2: String, measure3: String, measure4: String, measure5: String) {
         
         self.name = name
         self.id = id
@@ -61,30 +41,10 @@ struct Drink {
         self.ingredient3 = ingredients3
         self.ingredient4 = ingredients4
         self.ingredient5 = ingredients5
-        self.ingredient6 = ingredients6
-        self.ingredient7 = ingredients7
-        self.ingredient8 = ingredients8
-        self.ingredient9 = ingredients9
-        self.ingredient10 = ingredients10
-        self.ingredient11 = ingredients11
-        self.ingredient12 = ingredients12
-        self.ingredient13 = ingredients13
-        self.ingredient14 = ingredients14
-        self.ingredient15 = ingredients15
         self.measure1 = measure1
         self.measure2 = measure2
         self.measure3 = measure3
         self.measure4 = measure4
         self.measure5 = measure5
-        self.measure6 = measure6
-        self.measure7 = measure7
-        self.measure8 = measure8
-        self.measure9 = measure9
-        self.measure10 = measure10
-        self.measure11 = measure11
-        self.measure12 = measure12
-        self.measure13 = measure13
-        self.measure14 = measure14
-        self.measure15 = measure15
     }
 }
